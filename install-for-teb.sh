6126928021f5ffe38b8e45c39f735d781e3740b2
Your Name <you@example.com>
2020-08-20 17:02:57 +0000

    update

--- install-for-teb.sh
+++ install-for-teb.sh
@@ -2,3 +2,6 @@ sudo apt install -y ros-kinetic-libg2o
 sudo apt install -y ros-kinetic-costmap-converter
 
 sudo apt-get install -y ros-kinetic-jsk-visualization
+
+sudo apt-get install --no-install-recommends -y ros-kinetic-dwa-local-planner
+sudo apt-get install --no-install-recommends -y libarmadillo-dev libarmadillo6
